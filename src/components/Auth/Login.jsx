4f4b21abbfa01a4ad0f3921038f265a03abba1fc
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-20 19:13:16 +0500

    add to commit

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    
+
     const submitHandler = (e)=>  {
         e.preventDefault()
         console.log('Hello guyss, Form Submitted');
@@ -18,7 +18,12 @@ function Login() {
                 submitHandler(e)
             }} 
             className="flex flex-col items-center justify-center">
-                <input required className=" text-white outline-none bg-transparent border-2 border-emerald-600 rounded-full py-3 text-xl px-5 placeholder:text-gray-400" type="email" placeholder="Enter your email" />
+                <input
+                value={email}
+                onChange={(e)=> {
+                    setEmail(e.target.value)
+                }}
+                 required className=" text-white outline-none bg-transparent border-2 border-emerald-600 rounded-full py-3 text-xl px-5 placeholder:text-gray-400" type="email" placeholder="Enter your email" />
                 <input required className=" text-white outline-none mt-3 bg-transparent border-2 border-emerald-600 rounded-full py-3 text-xl px-5 placeholder:text-gray-400 " type="password" placeholder="Enter password" />
                 <button className=" text-white mt-5 outline-none border-none border-2 bg-emerald-600 rounded-full py-3 text-xl px-5 placeholder:text-white">Log in</button>
             </form>
