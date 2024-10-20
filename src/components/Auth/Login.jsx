f155fc871eeae5640f6932e77a1ebde7ef64a2a6
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-20 19:32:27 +0500

    update login

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -7,8 +7,11 @@ function Login() {
 
     const submitHandler = (e)=>  {
         e.preventDefault()
-        console.log('Hello guyss, Form Submitted');
+        console.log('email is', email);
+        console.log('password is', password);
         
+        setEmail('');
+        setPassword('');
     }
     return (
         <div className="flex h-screen w-screen   items-center justify-center">
@@ -24,7 +27,12 @@ function Login() {
                     setEmail(e.target.value)
                 }}
                  required className=" text-white outline-none bg-transparent border-2 border-emerald-600 rounded-full py-3 text-xl px-5 placeholder:text-gray-400" type="email" placeholder="Enter your email" />
-                <input required className=" text-white outline-none mt-3 bg-transparent border-2 border-emerald-600 rounded-full py-3 text-xl px-5 placeholder:text-gray-400 " type="password" placeholder="Enter password" />
+                <input
+                value={password}
+                onChange={(e)=> {
+                    setPassword(e.target.value)
+                }} 
+                required className=" text-white outline-none mt-3 bg-transparent border-2 border-emerald-600 rounded-full py-3 text-xl px-5 placeholder:text-gray-400 " type="password" placeholder="Enter password" />
                 <button className=" text-white mt-5 outline-none border-none border-2 bg-emerald-600 rounded-full py-3 text-xl px-5 placeholder:text-white">Log in</button>
             </form>
             </div>
