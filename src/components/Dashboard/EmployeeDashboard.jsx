59d56d304093e77df3090e0bbaa2b9dd2fdb7ddf
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-21 19:07:52 +0500

    add to commit

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -3,7 +3,7 @@ import Header from "../others/Header";
 
 function EmployeeDashboard () {
     return (
-        <div>
+        <div className="p-10 bg-[#1C1C1C] h-screen">
             <Header />
         </div>
 
--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Header() {
     return (
         <div className="flex items-end justify-between">
-            <h1>Hello <br/> username</h1>
-            <button>Log Out</button>
+            <h1 className="text-2xl font-medium">Hello <br/> <span className="text-3xl font-semibold">Username</span></h1>
+            <button className="bg-red-600 text-lg font-medium text-white px-5 py-2 rounded">Log Out</button>
         </div>
     )
 }
