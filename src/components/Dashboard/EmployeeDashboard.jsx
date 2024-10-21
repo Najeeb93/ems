5b97a2eba77be8e94b4e7ed63af5862729835cac
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-21 19:26:22 +0500

    update

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../others/Header";
+import TaskListNumber from "../others/TaskListNumber";
 
 function EmployeeDashboard () {
     return (
         <div className="p-10 bg-[#1C1C1C] h-screen">
             <Header />
+            <TaskListNumber/>
         </div>
 
     )
