30de255270e7ddad7275f106fe85966576f89d62
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-21 19:44:58 +0500

    update task list

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 function TaskList() {
     return (
-        <div className="h-[55%] flex items-center  justify-start gap-10 w-full py-5 bg-red-500 mt-10 ">
+        <div className="h-[55%] flex items-center  justify-start gap-5 flex-nowrap w-full py-5 bg-red-500 mt-10 ">
+            <div className="h-full w-[300px] bg-yellow-400 rounded-xl">
+
+            </div>
+
             <div className="h-full w-[300px] bg-yellow-400 rounded-xl">
 
             </div>
