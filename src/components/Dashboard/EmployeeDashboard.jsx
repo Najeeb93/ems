b493a8a657346506492e0975557c283a97531e42
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-21 18:55:04 +0500

    add to commit

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "../others/Header";
 
 function EmployeeDashboard () {
     return (
-        
+        <Header />
+
     )
 }
 
--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
     return (
-        <div>
+        <div className="flex items-center justify-end">
             <h1>Hello <br/> username</h1>
             <button>Log Out</button>
         </div>
