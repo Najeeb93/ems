c9c86632b0aa29c335c5331a6aba3fe57ce241ea
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-22 18:41:46 +0500

    add to commit task list

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -3,8 +3,11 @@ import React from "react";
 function TaskList() {
     return (
         <div id="tasklist" className="h-[55%] overflow-x-auto flex items-center  justify-start gap-5 flex-nowrap w-full py-5  mt-10 ">
-            <div className="flex-shrink-0 h-full w-[300px] bg-red-400 rounded-xl">
-
+            <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl">
+            <div className="flex justify-between items-center">
+                <h3 className="bg-red-600 px-3 py-1 rounded">High</h3>
+                <h4>20 Oct 2024</h4>
+            </div>
             </div>
             
 
