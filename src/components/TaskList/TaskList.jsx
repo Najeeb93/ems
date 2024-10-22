173570173e2157040bc96d987b9f77db7f98b554
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-22 18:31:16 +0500

    add to commit

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 function TaskList() {
     return (
-        <div className="h-[55%] flex items-center  justify-start gap-5 flex-nowrap w-full py-5 bg-red-500 mt-10 ">
+        <div className="h-[55%] overflow-x-auto flex items-center  justify-start gap-5 flex-nowrap w-full py-5  mt-10 ">
+            <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl">
+
+            </div>
+            <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl">
+
+            </div>
             <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl">
 
             </div>
