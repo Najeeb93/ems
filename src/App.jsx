c93fe44d751bb9154d35b3f89f05bfdd0493e555
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-22 19:10:58 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
    <>
    {/* <Login/> */}
-   <EmployeeDashboard/>
+   {/* <EmployeeDashboard/> */}
    </>
   )
 }
---
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function AdminDashboard () {
+    return (
+
+    )
+}
+
+
+export default AdminDashboard;
