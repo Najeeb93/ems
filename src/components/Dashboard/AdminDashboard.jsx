b8db570648c138ea2401c56b5fa640ea2a965b61
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-23 19:19:00 +0500

    update

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -15,12 +15,18 @@ function AdminDashboard () {
                    <h3>Description</h3>
                    <textarea name="" id="" cols="30" rows="10"></textarea>
                    </div>
+                    <div>
                     <h3>Date</h3>
                     <input type="date" />
+                    </div>
+                    <div>
                     <h3>Asign to</h3>
                     <input type="text" placeholder="Employee Name" />
+                    </div>
+                    <div>
                     <h3>Category</h3>
                     <input type="text" placeholder="design, dev, etc" />
+                    </div>
                     <button>Create Task</button>
                 </form>
             </div>
