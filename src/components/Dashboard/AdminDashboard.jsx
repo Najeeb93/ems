eda0ca58dc33b89f3d55cfb1a6a992b279d056cc
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-23 19:17:25 +0500

    add to changes

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -7,16 +7,21 @@ function AdminDashboard () {
             <Header/>
             <div>
                 <form >
+                    <div>
                     <h3>Task Title</h3>
                     <input type="text" placeholder="Make ui design" />
-                    <h3>Description</h3>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                    </div>
+                   <div>
+                   <h3>Description</h3>
+                   <textarea name="" id="" cols="30" rows="10"></textarea>
+                   </div>
                     <h3>Date</h3>
                     <input type="date" />
                     <h3>Asign to</h3>
                     <input type="text" placeholder="Employee Name" />
                     <h3>Category</h3>
                     <input type="text" placeholder="design, dev, etc" />
+                    <button>Create Task</button>
                 </form>
             </div>
         </div>
