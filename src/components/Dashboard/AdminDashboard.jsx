51ef5557ba3b89d1ffcb0a833eca66bf67fb68f2
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-25 18:12:36 +0500

    add to commit

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../others/Header";
 import CreateTask from "../others/CreateTask";
+import AllTask from "../others/AllTask";
 
 function AdminDashboard() {
   return (
     <div className="h-screen w-full p-7">
       <Header />
      <CreateTask />
+     <AllTask />
     </div>
   );
 }
--- src/components/others/AllTask.jsx
+++ src/components/others/AllTask.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 function AllTask() {
     return (
-            <div className="bg-[#1c1c1c] p-5">
-                <div>
+            <div className="bg-[#1c1c1c] p-5 rounded mt-5">
+                <div className="bg-red-400 py-2 px-4 flex justify-between">
                     <h2>Username</h2>
+                    <h3>Make a UI Design</h3>
+                    <h5>Status</h5>
                 </div>
 
             </div>
