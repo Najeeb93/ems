9dbb054cab9dc569b0153e74e25a280ffa4aa42d
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-25 17:54:11 +0500

    update file

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -3,37 +3,9 @@ import Header from "../others/Header";
 
 function AdminDashboard() {
   return (
-    <div className="h-screen w-full p-10">
+    <div className="h-screen w-full p-7">
       <Header />
-      <div className="p-5 bg-[#1c1c1c] mt-7 rounded">
-        <form className="flex items-start w-full flex-wrap  justify-between">
-            <div className="w-1/2">
-            <div>
-            <h3 className="text-sm text-gray-300 mb-0.5">Task Title</h3>
-            <input className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4" type="text" placeholder="Make ui design" />
-          </div>
-            <div>
-            <h3 className="text-sm text-gray-300 mb-0.5">Date</h3>
-            <input  className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"type="date" />
-          </div>
-          <div>
-            <h3 className="text-sm text-gray-300 mb-0.5">Asign to</h3>
-            <input className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4" type="text" placeholder="Employee Name" />
-          </div>
-          <div>
-            <h3 className="text-sm text-gray-300 mb-0.5">Category</h3>
-            <input className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4" type="text" placeholder="design, dev, etc" />
-          </div>
-            </div>
-         
-          <div className="w-2/5 flex flex-col items-start">
-            <h3 className="text-sm text-gray-300 mb-0.5">Description</h3>
-            <textarea className="w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-[1px] border-gray-400" name="" id="" ></textarea>
-          <button className="bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-4 w-full">Create Task</button>
-          </div>
-          
-        </form>
-      </div>
+     
     </div>
   );
 }
