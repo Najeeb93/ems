31046e3576bbba1023bbeb2b3473fd8405d6adb0
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-25 17:56:02 +0500

    update admin dashboard

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "../others/Header";
+import CreateTask from "../others/CreateTask";
 
 function AdminDashboard() {
   return (
     <div className="h-screen w-full p-7">
       <Header />
-     
+     <CreateTask />
     </div>
   );
 }
