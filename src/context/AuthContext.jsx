696f099c828f218249c3657210cfb1dacf5541e4
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-28 18:35:24 +0500

    update

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,9 +1,9 @@
-import React, { Children } from "react";
+import React from "react";
 
 function AuthContext({Children}) {
     return (
         <div>
-            {Children}
+       {Children}
         </div>
     )
 }
