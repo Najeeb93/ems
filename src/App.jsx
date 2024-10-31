7b5833b8b0db87f2f81594d98d0c9af7471d2361
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-31 18:46:03 +0500

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
+import { setLocalStorage } from './utlis/localStorge'
 
 function App() {
   
-
+  const [user, setUser] = useState(null);
   return (
    <>
    <Login/>
