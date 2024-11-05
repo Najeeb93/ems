999659a274f099b7679e9078e5ed184dfb448991
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-05 19:27:56 +0500

    update

--- src/components/others/TaskListNumber.jsx
+++ src/components/others/TaskListNumber.jsx
@@ -10,7 +10,7 @@ function TaskListNumber({data}) {
             </div>
 
             <div className="rounded-xl w-[45%] py-6 px-9 bg-blue-400 ">
-                <h2 className="text-3xl font-semibold">0</h2>
+                <h2 className="text-3xl font-semibold">{data.taskCounts.newTask}</h2>
                 <h3 className="text-xl font-medium">New Task</h3>
             </div>
 
