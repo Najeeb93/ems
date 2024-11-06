53c9f78a0c5cb4d4d12d23c5c3a7ed16d7efd682
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-06 18:26:11 +0500

    update

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -4,6 +4,7 @@ function AcceptTask() {
     return (
         <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl">
             <div className="flex justify-between items-center">
+                <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
 
             </div>
         </div>
