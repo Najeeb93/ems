758c3d36232eef8f562bb7350858d92e85a61093
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-06 18:53:32 +0500

    add to styling

--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 function NewTask() {
     return (
+        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl">
+            <div className="flex justify-between items-center">
+                
 
+            </div>
+
+        </div>
     )
 }
 
