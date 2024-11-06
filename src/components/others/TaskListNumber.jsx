787f1edb366808e6cc3a3082b6b24480f395c779
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-06 18:06:17 +0500

    add to commit

--- src/components/others/TaskListNumber.jsx
+++ src/components/others/TaskListNumber.jsx
@@ -6,7 +6,7 @@ function TaskListNumber({data}) {
         <div className="flex  mt-10 justify-between gap-5 screen">
 
             <div className="rounded-xl w-[45%] py-6 px-9 bg-blue-400 ">   
-                <h2 className="text-3xl font-semibold">{data.taskCounts.new}</h2>
+                <h2 className="text-3xl font-semibold">{data.taskCounts.newTask}</h2>
                 <h3 className="text-xl font-medium">New Task</h3>
             </div>
 
