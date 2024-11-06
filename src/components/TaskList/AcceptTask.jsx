dd462a91012d63169018fc224f66d4cda2a1eba7
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-06 18:28:27 +0500

    add to commit

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -5,8 +5,9 @@ function AcceptTask() {
         <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl">
             <div className="flex justify-between items-center">
                 <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
-
+                <h4 className="text-sm">20 feb 2024</h4>
             </div>
+            <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
         </div>
     )
 }
