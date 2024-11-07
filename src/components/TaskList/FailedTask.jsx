9332cdee151b0e924c9f07cc0db485d1f0901348
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-07 18:59:32 +0500

    add to commit failed file

--- src/components/TaskList/FailedTask.jsx
+++ src/components/TaskList/FailedTask.jsx
@@ -5,8 +5,10 @@ function FailedTask() {
     return (
         <div className="flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl">
             <div className="flex justify-between items-center">
-
+                <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+                <h4 className="text-sm">20 feb 2024</h4>
             </div>
+            
         </div>
     )
 }
