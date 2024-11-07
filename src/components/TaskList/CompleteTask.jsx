d82c86be205037ccc0ac95edf126ee16f887a944
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-07 18:30:30 +0500

    add to changes

--- src/components/TaskList/CompleteTask.jsx
+++ src/components/TaskList/CompleteTask.jsx
@@ -3,7 +3,12 @@ import React  from "react";
 
 function CompleteTask() {
     return (
+        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl">
+            <div className="flex justify-between items-center">
 
+            </div>
+
+        </div>
     )
 }
 
