7fe630bd71f4a5d28ba70b172d9c49a23868d9bc
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-07 18:56:54 +0500

    update

--- src/components/TaskList/FailedTask.jsx
+++ src/components/TaskList/FailedTask.jsx
@@ -3,7 +3,11 @@ import React from "react";
 
 function FailedTask() {
     return (
+        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl">
+            <div className="flex justify-between items-center">
 
+            </div>
+        </div>
     )
 }
 
