15160dd31d7db9c5d84ddc0c69fc81498493e992
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-08 18:42:52 +0500

    add to commit

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import AcceptTask from "./AcceptTask";
 
 function TaskList({data}) {
     return (
         <div id="tasklist" className="h-[50%] overflow-x-auto flex items-center  justify-start gap-5 flex-nowrap w-full py-1  mt-16 ">
-         
+         {data.tasks.map((elem, idx)=>{
+            if(elem.active) {
+                return <AcceptTask key={idx} data={elem} />
+            }
+         })}
 
            
         </div>
