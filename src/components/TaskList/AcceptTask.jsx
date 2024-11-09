f48fbad80c32700132fbddc4eb2ae169999198b3
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-09 13:50:24 +0500

    update

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -4,12 +4,12 @@ function AcceptTask({data}) {
     return (
         <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl">
             <div className="flex justify-between items-center">
-                <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+                <h3 className="bg-red-600 text-sm px-3 py-1 rounded">{data.category}</h3>
                 <h4 className="text-sm">20 feb 2024</h4>
             </div>
             <h2 className="mt-5 text-2xl font-semibold">{data.taskTitle}</h2>
             <p className="text-sm mt-2">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi dicta deleniti voluptas adipisci, eaque ipsa illum itaque voluptate repudiandae. Fugiat dolorem sapiente doloremque excepturi molestiae magni, ullam repellendus vel omnis.
+              {data.taskDescription}
             </p>
             <div className="flex justify-between mt-4">
                 <button className="bg-green-500 py-1 px-2 text-sm">Mark as Completed</button>
