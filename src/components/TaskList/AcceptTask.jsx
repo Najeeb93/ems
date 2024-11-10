c2c47496b15a7c29fc57b73c27e6d18f598ff434
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-10 14:20:54 +0500

    add to commit

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -5,7 +5,7 @@ function AcceptTask({data}) {
         <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl">
             <div className="flex justify-between items-center">
                 <h3 className="bg-red-600 text-sm px-3 py-1 rounded">{data.category}</h3>
-                <h4 className="text-sm">20 feb 2024</h4>
+                <h4 className="text-sm">{data.taskDate}</h4>
             </div>
             <h2 className="mt-5 text-2xl font-semibold">{data.taskTitle}</h2>
             <p className="text-sm mt-2">
