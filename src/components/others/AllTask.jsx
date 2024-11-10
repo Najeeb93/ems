0dbf90afc370088d48827f7c23d3632eac07d620
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-10 19:56:52 +0500

    update all task file

--- src/components/others/AllTask.jsx
+++ src/components/others/AllTask.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../context/AuthProvider";
 
 function AllTask() {
+
+    const authData = useContext(AuthContext)
     return (
             <div className="bg-[#1c1c1c] p-5 rounded mt-5 h-48 overflow-auto">
                 <div className="bg-red-400 mb-2  py-2 px-4 flex justify-between rounded">
