5bbb27699bdfcaaaf7259a4cf455978fac39ed2e
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-10 19:51:04 +0500

    add to commit

--- src/components/TaskList/CompleteTask.jsx
+++ src/components/TaskList/CompleteTask.jsx
@@ -6,7 +6,7 @@ function CompleteTask({data}) {
         <div className="flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl">
             <div className="flex justify-between items-center">
                 <h3 className="bg-red-600 text-sm px-3 py-1 rounded">{data.category}</h3>
-                <h4 className="text-sm">20 feb 2024</h4>
+                <h4 className="text-sm">{data.taskDate}</h4>
             </div>
             <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
             <p className="text-sm mt-2">
