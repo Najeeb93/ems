ddb21a68235224c66c329d404fd1ccce1a7556c6
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-10 14:29:44 +0500

    update

--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -5,7 +5,7 @@ function NewTask({data}) {
         <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl">
             <div className="flex justify-between items-center">
                 <h3 className="bg-red-600 text-sm px-3 py-1 rounded">{data.category}</h3>
-                <h4 className="text-sm">20 feb 2024</h4>
+                <h4 className="text-sm">{data.taskDate}</h4>
 
             </div>
             <h2 className="mt-5 text-2xl font-semibold">{data.taskTitle}</h2>
