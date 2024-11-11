181f6e6f81b4e193c3f2e7c1a329a5b9ff6852e8
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-11 13:21:22 +0500

    add to commit

--- src/components/others/AllTask.jsx
+++ src/components/others/AllTask.jsx
@@ -13,26 +13,9 @@ function AllTask() {
                     <h5 className="text-lg font-medium w-1/5">Completed</h5>
                     <h5 className="text-lg font-medium w-1/5">Failed</h5>
                 </div>
-                <div className="bg-green-400 mb-2 py-2 px-4 flex justify-between rounded">
-                    <h2>Username</h2>
-                    <h3>Make a UI Design</h3>
-                    <h5>Status</h5>
-                </div>
-                <div className="bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded">
-                    <h2>Username</h2>
-                    <h3>Make a UI Design</h3>
-                    <h5>Status</h5>
-                </div>
-                <div className="bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded">
-                    <h2>Username</h2>
-                    <h3>Make a UI Design</h3>
-                    <h5>Status</h5>
-                </div>
-                <div className="bg-purple-400 mb-2 py-2 px-4 flex justify-between rounded">
-                    <h2>Username</h2>
-                    <h3>Make a UI Design</h3>
-                    <h5>Status</h5>
-                </div>
+              
+             
+              
 
             </div>
     )
