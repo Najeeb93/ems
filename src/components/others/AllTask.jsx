b266ba3e5c7230bbd9c22bad780cccb05caefe76
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-11 18:53:25 +0500

    update

--- src/components/others/AllTask.jsx
+++ src/components/others/AllTask.jsx
@@ -19,6 +19,7 @@ function AllTask() {
                 <h2 className="w-1/5">{elem.firstName}</h2>
                 <h3 className="w-1/5 text-blue-400">{elem.taskCounts.newTask}</h3>
                 <h5 className="w-1/5 text-yellow-400">{elem.taskCounts.active}</h5>
+                <h5 className="w-1/5 text-white">{elem.taskCounts.completed}</h5>
             </div>
           );
         })}
