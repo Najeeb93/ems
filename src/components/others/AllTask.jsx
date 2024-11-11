5c444862e1667ac3518ad15f638013fdb067924c
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-11 12:55:41 +0500

    add to commit

--- src/components/others/AllTask.jsx
+++ src/components/others/AllTask.jsx
@@ -7,7 +7,7 @@ function AllTask() {
     return (
             <div className="bg-[#1c1c1c] p-5 rounded mt-5">
                 <div className="bg-red-400 mb-2  py-2 px-4 flex justify-between rounded">
-                    <h2>Username</h2>
+                    <h2 className="text-lg font-medium w-1/5">Employee Name</h2>
                     <h3>Make a UI Design</h3>
                     <h5>Status</h5>
                 </div>
