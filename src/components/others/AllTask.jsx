13ebd85eda96fb06f9915aac8cf0158f946b8878
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-11 13:01:22 +0500

    add to commit

--- src/components/others/AllTask.jsx
+++ src/components/others/AllTask.jsx
@@ -9,7 +9,7 @@ function AllTask() {
                 <div className="bg-red-400 mb-2  py-2 px-4 flex justify-between rounded">
                     <h2 className="text-lg font-medium w-1/5">Employee Name</h2>
                     <h3 className="text-lg font-medium w-1/5">New Task</h3>
-                    <h5>Status</h5>
+                    <h5 className="text-lg font-medium w-1/5">Active Task</h5>
                 </div>
                 <div className="bg-green-400 mb-2 py-2 px-4 flex justify-between rounded">
                     <h2>Username</h2>
