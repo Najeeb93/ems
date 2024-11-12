9b578b604850f61b2b805be376b33fe9d113bf68
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-12 18:00:51 +0500

    add to commit

--- src/components/others/CreateTask.jsx
+++ src/components/others/CreateTask.jsx
@@ -9,6 +9,7 @@ function CreateTask() {
 
   const [taskTitle, setTaskTitle] = useState('')
   const [taskDescription, setTaskDescription] = useState('')
+  const [taskDate, setTaskDate] = useState('')
     return (
         <div className="p-5 bg-[#1c1c1c] mt-7 rounded">
         <form className="flex items-start w-full flex-wrap  justify-between">
