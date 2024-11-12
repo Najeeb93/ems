79666c615c7213febdbbab9bd37b78579a477068
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-12 18:40:41 +0500

    update

--- src/components/others/CreateTask.jsx
+++ src/components/others/CreateTask.jsx
@@ -36,7 +36,12 @@ function CreateTask() {
           </div>
           <div>
             <h3 className="text-sm text-gray-300 mb-0.5">Asign to</h3>
-            <input className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4" type="text" placeholder="Employee Name" />
+            <input
+             value={asignTo}
+             onChange={(e)=>{
+               setAsignTo(e.target.value)
+             }}
+             className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4" type="text" placeholder="Employee Name" />
           </div>
           <div>
             <h3 className="text-sm text-gray-300 mb-0.5">Category</h3>
