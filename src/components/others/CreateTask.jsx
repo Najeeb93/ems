7f8abb92912b1f02c09f53013273d4ec8c2bb133
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-12 18:25:40 +0500

    add to commit

--- src/components/others/CreateTask.jsx
+++ src/components/others/CreateTask.jsx
@@ -27,7 +27,12 @@ function CreateTask() {
           </div>
             <div>
             <h3 className="text-sm text-gray-300 mb-0.5">Date</h3>
-            <input  className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"type="date" />
+            <input
+               value={taskDate}
+               onChange={(e)=>{
+                 setTaskDate(e.target.value)
+               }}
+             className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"type="date" />
           </div>
           <div>
             <h3 className="text-sm text-gray-300 mb-0.5">Asign to</h3>
