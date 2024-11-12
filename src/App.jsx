f02e4a7989c59d70aeca997cdc5d92c4dd49add9
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-12 19:01:36 +0500

    add to commit

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ useEffect(() => {
   return (
    <>
    {!user ? <Login handleLogin={handleLogin}/>: ''}
-   {user == 'admin' ? <AdminDashboard/> : (user == 'employee' ?  <EmployeeDashboard data={loggedInUserData}/> : null)} 
+   {user == 'admin' ? <AdminDashboard changeUser={setUser}/> : (user == 'employee' ?  <EmployeeDashboard changeUser={setUser} data={loggedInUserData}/> : null)} 
    </>
   )
 }
