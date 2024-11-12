8a8c33f970a03e4db69e18ca285deab8188a51fd
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-12 18:18:57 +0500

    update

--- src/components/others/CreateTask.jsx
+++ src/components/others/CreateTask.jsx
@@ -18,7 +18,12 @@ function CreateTask() {
             <div className="w-1/2">
             <div>
             <h3 className="text-sm text-gray-300 mb-0.5">Task Title</h3>
-            <input className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4" type="text" placeholder="Make ui design" />
+            <input
+            value={taskTitle}
+            onChange={(e)=>{
+              setTaskTitle(e.target.value)
+            }}
+             className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4" type="text" placeholder="Make ui design" />
           </div>
             <div>
             <h3 className="text-sm text-gray-300 mb-0.5">Date</h3>
