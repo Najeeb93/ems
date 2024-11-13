7beeb4d0b69e87497f12e1a749ad5bd8b81bdda4
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-11-13 18:24:49 +0500

    update

--- src/components/others/AllTask.jsx
+++ src/components/others/AllTask.jsx
@@ -13,7 +13,7 @@ function AllTask() {
         <h5 className="text-lg font-medium w-1/5">Failed</h5>
       </div>
       <div>
-        {authData.employee.map(function (elem,idx) {
+        {userData.map(function (elem,idx) {
           return (
             <div key={idx} className="border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded">
                 <h2 className="text-lg font-medium w-1/5">{elem.firstName}</h2>
